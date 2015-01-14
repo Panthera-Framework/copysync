3731fe1b5d9ee507d1fcbadef6fc3e461d782c05
Damian Kęska <damian.keska@fingo.info>
2015-01-14 17:26:11 +0100

    Added uncommited files dialog + fixed case when dialog list is empty

--- usr/share/copysync/plugins/git.py
+++ usr/share/copysync/plugins/git.py
@@ -27,16 +27,20 @@ class gitPlugin:
         ## make application output silently for a moment
         f = open(os.devnull, 'w')
         __stdout = sys.stdout
-        sys.stdout = f
+        #sys.stdout = f
 
         code, tag = self.dialog.menu("Options",
                        choices=[("(1)", "Add untracked files to queue"),
-                                ("(2)", "Cancel")])
+                                ("(2)", "Add uncommited files to queue"),
+                                ("(3)", "Cancel")])
 
         if tag == '(1)':
             self.selectAllMenu(self.untrackedFilesMenu)
+        elif tag == '(2)':
+            self.selectAllMenu(self.uncommitedFilesMenu)
 
         sys.stdout = __stdout
+
 
     def selectAllMenu(self, callback):
         code, tag = self.dialog.menu("Start with",
@@ -50,7 +54,7 @@ class gitPlugin:
         return callback(checked=checked)
 
 
-    def untrackedFilesMenu(self, checked=1):
+    def untrackedFilesMenu(self, checked = 1, onlyReturn = False):
         """
         Display menu with list of untracked files to select to add to queue
         :return:
@@ -70,7 +74,13 @@ class gitPlugin:
 
             choices.append((str(line.replace("\t", '')), "", checked))
 
-        code, tags = self.dialog.checklist("Select files:", choices=choices, width=100, height=25, list_height=15)
+        if onlyReturn:
+            return choices
+
+        if not choices:
+            return self.gitMenu()
+
+        code, tags = self.dialog.checklist("Select untracked files:", choices=choices, width=100, height=25, list_height=15)
 
         if tags:
             for file in tags:
@@ -78,3 +88,40 @@ class gitPlugin:
         else:
             self.gitMenu()
 
+
+    def uncommitedFilesMenu(self, checked = 1, onlyReturn = False):
+        """
+        Display menu with list of uncommited files to select to add to queue
+        :param bool|int checked:
+        :return:
+        """
+
+        choices = []
+
+        os.chdir(self.kernel.localDirectory)
+        gitOutput = subprocess.check_output('git status', shell=True)
+
+        posStart = gitOutput.find("to update what will be committed)\n")
+        block = gitOutput[posStart:]
+
+        print(block.split("\n")[3:])
+
+        for line in block.split("\n")[3:]:
+            if not line:
+                break
+
+            choices.append((str(line.replace("\t", '').replace('modified:   ', '')), "", checked))
+
+        if onlyReturn:
+            return choices
+
+        if not choices:
+            return self.gitMenu()
+
+        code, tags = self.dialog.checklist("Select uncommited files:", choices=choices, width=100, height=25, list_height=15)
+
+        if tags:
+            for file in tags:
+                self.kernel.appendToQueue(file)
+        else:
+            self.gitMenu()
