f2ff517429d4a5d921c2a36af93968ca7429c7a3
Damian Kęska <damian.keska@fingo.info>
2015-01-15 17:14:40 +0100

    Improved git menu

--- usr/share/copysync/plugins/git.py
+++ usr/share/copysync/plugins/git.py
@@ -27,34 +27,45 @@ class gitPlugin:
         ## make application output silently for a moment
         f = open(os.devnull, 'w')
         __stdout = sys.stdout
-        #sys.stdout = f
+        sys.stdout = f
 
         code, tag = self.dialog.menu("Options",
-                       choices=[("(1)", "Add untracked files to queue"),
-                                ("(2)", "Add uncommited files to queue"),
-                                ("(3)", "Cancel")])
+                       choices=[("(1)", "Add untracked/uncommited files to queue"),
+                                ("(2)", "Cancel")])
 
         if tag == '(1)':
-            self.selectAllMenu(self.untrackedFilesMenu)
-        elif tag == '(2)':
-            self.selectAllMenu(self.uncommitedFilesMenu)
+            self.selectAllMenu(callback = self.notCommitedFilesMenu, notCommitedMenu = True)
 
         sys.stdout = __stdout
 
 
-    def selectAllMenu(self, callback):
-        code, tag = self.dialog.menu("Start with",
-                       choices=[("(1)", "All positions checked"),
-                                ("(2)", "All positions unchecked")])
+    def selectAllMenu(self, callback, notCommitedMenu = False):
+
+        choices = [
+            ("(1)", "All positions checked"),
+            ("(2)", "All positions unchecked")
+        ]
+
+        if notCommitedMenu:
+            choices.append(("(3)", "Check only tracked"))
+            choices.append(("(4)", "Check only not commited"))
+
+        code, tag = self.dialog.menu("Start with", choices=choices)
         checked = 0
 
         if tag == '(1)':
             checked = 1
 
+        if notCommitedMenu:
+            if tag == '(3)':
+                return callback(checked = 0, commitedChecked = 0, untrackedChecked = 1)
+            elif tag == '(4)':
+                return callback(checked = 0, commitedChecked = 1, untrackedChecked = 0)
+
         return callback(checked=checked)
 
 
-    def untrackedFilesMenu(self, checked = 1, onlyReturn = False):
+    def notCommitedFilesMenu(self, checked = 1, commitedChecked = None, untrackedChecked = None):
         """
         Display menu with list of untracked files to select to add to queue
         :return:
@@ -65,6 +76,10 @@ class gitPlugin:
         os.chdir(self.kernel.localDirectory)
         gitOutput = subprocess.check_output('git status', shell=True)
 
+        # untracked files
+        if untrackedChecked is not None:
+            checked = untrackedChecked
+
         posStart = gitOutput.find("(use \"git add <file>...\" to include in what will be committed)\n\n")
         untrackedBlock = gitOutput[posStart:]
 
@@ -74,37 +89,12 @@ class gitPlugin:
 
             choices.append((str(line.replace("\t", '')), "", checked))
 
-        if onlyReturn:
-            return choices
-
-        if not choices:
-            return self.gitMenu()
-
-        code, tags = self.dialog.checklist("Select untracked files:", choices=choices, width=100, height=25, list_height=15)
-
-        if tags:
-            for file in tags:
-                self.kernel.appendToQueue(file)
-        else:
-            self.gitMenu()
-
-
-    def uncommitedFilesMenu(self, checked = 1, onlyReturn = False):
-        """
-        Display menu with list of uncommited files to select to add to queue
-        :param bool|int checked:
-        :return:
-        """
-
-        choices = []
-
-        os.chdir(self.kernel.localDirectory)
-        gitOutput = subprocess.check_output('git status', shell=True)
+        # not commited files
+        if commitedChecked is not None:
+            checked = commitedChecked
 
         posStart = gitOutput.find("to update what will be committed)\n")
         block = gitOutput[posStart:]
-
-        print(block.split("\n")[3:])
 
         for line in block.split("\n")[3:]:
             if not line:
@@ -112,13 +102,10 @@ class gitPlugin:
 
             choices.append((str(line.replace("\t", '').replace('modified:   ', '')), "", checked))
 
-        if onlyReturn:
-            return choices
-
         if not choices:
             return self.gitMenu()
 
-        code, tags = self.dialog.checklist("Select uncommited files:", choices=choices, width=100, height=25, list_height=15)
+        code, tags = self.dialog.checklist("Select untracked files:", choices=choices, width=100, height=25, list_height=15)
 
         if tags:
             for file in tags:
