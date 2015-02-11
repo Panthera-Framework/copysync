9997fdbc1a62293c63304384535afc35ad7654e7
Damian Kęska <damian.keska@fingo.info>
2015-02-11 12:07:41 +0100

    Added "history" plugin (type: [h] + [enter] to trigger when plugin activated)

--- usr/share/copysync/plugins/history.py
+++ usr/share/copysync/plugins/history.py
@@ -1,3 +1,9 @@
+#-*- encoding: utf-8 -*-
+import os
+import sys
+from dialog import Dialog
+import time
+
 """
 Simple history showing plugin for copysync
 
@@ -11,6 +17,8 @@ class historyPlugin:
     :author: Damian Kęska <damian.keska@fingo.pl>
     """
 
+    history = ''
+
     def __init__(self, app):
         """
         Initialize - append callbacks to copysync functions
@@ -19,6 +27,40 @@ class historyPlugin:
         """
 
         self.kernel = app
+        self.dialog = Dialog(dialog="dialog")
 
-        self.kernel.hooking.addOption('app.syncJob.Queue.iterate', self.queueAction, priority=99)
-        self.kernel.hooking.addOption('app.syncJob.Queue.iterate.item', self.queueFileAction, priority=99)
+        # hook up to queue
+        self.kernel.hooking.addOption('app.syncJob.Queue.iterate.item', self.queueFileAction, priority=10)
+
+        # hook up to interactive console
+        if self.kernel.interactive:
+            self.kernel.interactive.recognizedChars['h'] = self.showHistoryWindow
+
+    def queueFileAction(self, args):
+        """
+        Capture queue actions to put into history log
+        """
+
+
+        if not 'operationType' in args or not args['operationType']:
+            return args
+
+        self.history += "["+time.strftime("%d.%m.%Y %H:%M:%S")+"] " + args['operationType'] + " " +args['virtualPath']+ "\n"
+
+        return args
+
+    def showHistoryWindow(self):
+        """
+        Show history window using Dialog
+        :return:
+        """
+
+
+        f = open(os.devnull, 'w')
+        __stdout = sys.stdout
+        sys.stdout = f
+
+        self.dialog.msgbox(self.history, width=160, height=30)
+
+        # restore stdout
+        sys.stdout = __stdout
